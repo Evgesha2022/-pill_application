59319731fc8c57ef12de7225658ecff693bba1ee
ВАРВАРА КАРЯКИНА <honor07092021zel@outlook.com>
2023-05-28 23:44:18 +0300

    updated layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,9 @@
-import { Link , Outlet} from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { Button } from "@salutejs/plasma-ui";
-import { IconAvatar, IconApps, IconCartAlt,IconHeart, IconPlus} from "@salutejs/plasma-icons";
+import { IconAvatar, IconApps, IconCartAlt, IconHeart, IconPlus } from "@salutejs/plasma-icons";
 import "../App.css"
+import "./menu.css"
+
 //внизу страниы меню как сделать
 /*async function Error (){
     if(!localStorage.getItem('user'))
@@ -9,24 +11,24 @@ import "../App.css"
     else{ return '/addtablet';}
    }*/
 
-const Layout =  () =>{
-  
-   //console.log("Error", Error())
-    return(
-        <>
-      <div className="parent" >
-        <ul  >
-            <li  className="left"> <Link  to='/'><Button  pin="circle-circle" contentLeft={<IconApps/>} 
-     /></Link></li>
-    <li className="left" ><Link   to='/tablets'><Button   pin="circle-circle" contentLeft={<IconHeart/>} /></Link></li>
-    <li className="right"><Link to='/profile'><Button  pin="circle-circle"  contentLeft={<IconAvatar/>}/></Link></li>
-    <li className="right"><Link  to='/eapteka'><Button   pin="circle-circle" contentLeft={<IconCartAlt/>} /></Link></li>
-    <li className="right"><Link  to={'/addtablet'}><Button  pin="circle-circle" contentLeft={<IconPlus/>} /></Link></li>
-    </ul>
-    </div>
-    <Outlet/>
+const Layout = () => {
+
+  //console.log("Error", Error())
+  return (
+    <>
+      <div className="parent" style={{}}>
+        <ul style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}  >
+          <li className="left" style={{ flexBasis: '25%', textAlign: 'center' }}> <Link to='/'><Button pin="circle-circle" contentLeft={<IconApps />}
+          /></Link></li>
+          <li className="left" style={{ flexBasis: '25%', textAlign: 'center' }} ><Link to='/tablets'><Button pin="circle-circle" contentLeft={<IconHeart />} /></Link></li>
+          <li className="right" style={{ flexBasis: '25%', textAlign: 'center' }}><Link to='/profile'><Button pin="circle-circle" contentLeft={<IconAvatar />} /></Link></li>
+          <li className="right" style={{ flexBasis: '25%', textAlign: 'center' }}><Link to='/eapteka'><Button pin="circle-circle" contentLeft={<IconCartAlt />} /></Link></li>
+          <li className="right" style={{ flexBasis: '25%', textAlign: 'center' }}><Link to={'/addtablet'}><Button pin="circle-circle" contentLeft={<IconPlus />} /></Link></li>
+        </ul>
+      </div>
+      <Outlet />
     </>
-         
-    )
- }
- export {Layout}
+
+  )
+}
+export { Layout }
